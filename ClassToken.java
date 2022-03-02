827b5bda05cdb52b083e133167163dc102e3297e
PitmanFlippin <65986643+enp0s10@users.noreply.github.com>
2022-03-02 11:45:10 -0800

    My Token Files didn't upload.
    Still learning eclipse.

---
+++ ClassToken.java
@@ -0,0 +1,14 @@
+public class ClassToken implements Token {
+	public boolean equals(final Object other) {
+		return other instanceof ClassToken;
+	}
+	
+	public int hashCode() {
+		return 14;
+	}
+	
+	public String toString() {
+		return "class";
+	}
+}
+
---
+++ OpenparToken.java
@@ -0,0 +1,14 @@
+public class OpenparToken implements Token {
+	public boolean equals(final Object other) {
+		return other instanceof OpenparToken;
+	}
+	
+	public int hashCode() {
+		return 10;
+	}
+	
+	public String toString() {
+		return "(";
+	}
+}
+
