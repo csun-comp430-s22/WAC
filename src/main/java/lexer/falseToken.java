fd879b9970dd0b779ec0778e23b6f4a8db79cf6f
rubencmd <perez.ruben19@yahoo.com>
2022-03-03 01:56:28 -0800

    Here are my tokens

---
+++ src/main/java/lexer/falseToken.java
@@ -0,0 +1,16 @@
+package main.java.lexer;
+
+public class falseToken {
+
+    public boolean equals(final Object other) {
+        return other instanceof falseToken;
+    }
+
+    public int hashCode() {
+        return 26;
+    }
+
+    public String toString() {
+        return "false";
+    }
+}
