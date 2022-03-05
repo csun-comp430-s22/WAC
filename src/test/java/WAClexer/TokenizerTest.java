0d4e5b4ca11ef1ed658a2589af380cef43494b70
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-03-04 17:21:40 -0800

    added junit tests

--- src/test/java/WAClexer/TokenizerTest.java
+++ src/test/java/WAClexer/TokenizerTest.java
@@ -2,31 +2,43 @@ package WAClexer;
 
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TokenizerTest {
-	public static void testEmptyString() throws TokenizerException {
+	//annotation
+	@Test
+	public void testEmptyString() throws TokenizerException {
 		//check that tokenizing empty string works
  		Tokenizer tokenizer = new Tokenizer("");
 		List<Token> tokens = tokenizer.tokenize();
-		assert(tokens.size() == 0);
+		assertEquals(0, tokens.size());
+		//assert(tokens.size() == 0);
 	}
 	
-	public static void testOnlyWhitespace() throws TokenizerException {
+	@Test
+	public void testOnlyWhitespace() throws TokenizerException {
 		Tokenizer tokenizer = new Tokenizer("    ");
 		List<Token> tokens = tokenizer.tokenize();
-		assert(tokens.size() == 0);
+		assertEquals(0, tokens.size());
+		//assert(tokens.size() == 0);
 	}
 	
-	public static void testIntByItself() throws TokenizerException {
+	@Test
+	public void testIntByItself() throws TokenizerException {
 		Tokenizer tokenizer = new Tokenizer("Int");
 		List<Token> tokens = tokenizer.tokenize();
-		assert(tokens.size() == 1);
+		assertEquals(1, tokens.size());
+		//assert(tokens.size() == 1);
 		Token intToken = tokens.get(0);
-		assert(intToken instanceof IntToken);
+		assertTrue(intToken instanceof IntToken);
+		//assert(intToken instanceof IntToken);
 	}
 	
-	public static void main(String[] args) throws TokenizerException {
+/* 	public static void main(String[] args) throws TokenizerException {
 		testOnlyWhitespace();
 		testEmptyString();
 		testIntByItself();
-	}
+	} */
 }
