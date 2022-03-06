663bf765c98e70eeedd5e6c971f6e941288ead22
shadowwolf777180 <cesarbarrera173@gmail.com>
2022-03-06 01:27:56 -0800

    Tokenizer part added for cesar

--- src/main/java/WAClexer/Tokenizer.java
+++ src/main/java/WAClexer/Tokenizer.java
@@ -50,7 +50,30 @@ public class Tokenizer {
 			} else if (input.startsWith("super", offset)) {
 				offset += 5;
 				return new SuperToken();
-			} else {
+			} 
+			else if (input.startsWith("constructor", offset)) {
+				offset += 11;
+				return new ConstructorToken();
+			} else if (input.startsWith("while", offset)) {
+				offset += 5;
+				return new WhileToken();
+			}  else if (input.startsWith("break", offset)) {
+				offset += 5;
+				return new BreakToken();
+			}  else if (input.startsWith("if", offset)) {
+				offset += 2;
+				return new IfToken();
+			}  else if (input.startsWith("else", offset)) {
+				offset += 4;
+				return new ElseToken();
+			}  else if (input.startsWith("return", offset)) {
+				offset += 5;
+				return new ReturnToken();
+			}  else if (input.startsWith("{", offset)) {
+				offset += 1;
+				return new LeftBracketToken();
+			}  
+				else {
 				throw new TokenizerException();
 			}
 		} else {
