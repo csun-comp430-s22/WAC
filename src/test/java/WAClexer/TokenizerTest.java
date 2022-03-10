f2bb8e41705b7d1f98e065490c1f47b90cea5a59
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-03-09 17:56:09 -0800

    trying to make tests =_=

--- src/test/java/WAClexer/TokenizerTest.java
+++ src/test/java/WAClexer/TokenizerTest.java
@@ -4,41 +4,72 @@ import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class TokenizerTest {
+	public void assertTokenizes(final String input, final Token[] expected)
+				throws TokenizerException {
+			final Tokenizer tokenizer = new Tokenizer(input);
+			final List<Token> received = tokenizer.tokenize();
+			assertArrayEquals(expected, received.toArray(new Token[received.size()]));
+	}
+	
 	//annotation
 	@Test
 	public void testEmptyString() throws TokenizerException {
 		//check that tokenizing empty string works
- 		Tokenizer tokenizer = new Tokenizer("");
-		List<Token> tokens = tokenizer.tokenize();
-		assertEquals(0, tokens.size());
-		//assert(tokens.size() == 0);
+		assertTokenizes("", new Token[0]);
+
+ 		//Tokenizer tokenizer = new Tokenizer("");
+		//List<Token> tokens = tokenizer.tokenize();
+		//assertEquals(0, tokens.size());
 	}
 	
 	@Test
-	public void testOnlyWhitespace() throws TokenizerException {
-		Tokenizer tokenizer = new Tokenizer("    ");
-		List<Token> tokens = tokenizer.tokenize();
-		assertEquals(0, tokens.size());
-		//assert(tokens.size() == 0);
+	public void testOnlyWhitespace() throws TokenizerException{
+		assertTokenizes("    ", new Token[0]);
+		
+		//Tokenizer tokenizer = new Tokenizer("    ");
+		//List<Token> tokens = tokenizer.tokenize();
+		//assertEquals(0, tokens.size());
 	}
 	
 	@Test
 	public void testIntByItself() throws TokenizerException {
+		//assertTokenizes("Int", new Token[] { new IntToken() });
+		
 		Tokenizer tokenizer = new Tokenizer("Int");
 		List<Token> tokens = tokenizer.tokenize();
 		assertEquals(1, tokens.size());
-		//assert(tokens.size() == 1);
 		Token intToken = tokens.get(0);
 		assertTrue(intToken instanceof IntToken);
-		//assert(intToken instanceof IntToken);
 	}
 	
-/* 	public static void main(String[] args) throws TokenizerException {
-		testOnlyWhitespace();
-		testEmptyString();
-		testIntByItself();
+	@Test
+	public void testVariable() throws TokenizerException {
+		assertTokenizes("foo", new Token[] { new VariableToken("foo") });
+	}
+	
+	@Test
+	public void testIntIntIsVariable() throws TokenizerException {
+		assertTokenizes("IntInt", new Token[]{ new VariableToken("IntInt") });
+	}
+	
+/* 	@Test
+	public void testPlusByItself() throws TokenizerException {
+		assertTokenizes("+", new Token[] { new PlusToken() });
+	} */
+	
+/* 	@Test
+	public void testIntSpaceIntAreIntTokens() {
+		assertTokenizes("Int Int", new Token[]{ new IntToken(), new IntToken() });
+	} */
+	
+/* 	@Test
+	public void testBooleanByItself() throws TokenizerException {
+		assertTokenizes("Boolean", new Token[] { new BooleanToken() });
 	} */
 }
