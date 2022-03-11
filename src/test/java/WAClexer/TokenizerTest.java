33cf878642032e1b2afd02c3778d5d8411451ea1
shadowwolf777180 <cesarbarrera173@gmail.com>
2022-03-11 11:49:35 -0800

    Took out import

--- src/test/java/WAClexer/TokenizerTest.java
+++ src/test/java/WAClexer/TokenizerTest.java
@@ -1,7 +1,7 @@
 package WAClexer;
 
 import java.util.List;
-import java.util.ArrayList;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
