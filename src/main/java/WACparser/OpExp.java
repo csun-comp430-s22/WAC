d684d0d18119709e6121586d550faea34b91fad4
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-03-28 16:55:58 -0700

    added methods for OpExp1

--- src/main/java/WACparser/OpExp.java
+++ src/main/java/WACparser/OpExp.java
@@ -10,4 +10,21 @@ public class OpExp implements Exp {
 		this.op = op;
 		this.right = right;
 	}
+	
+	public boolean equals(final Object other) {
+		if (other instanceof OpExp) {
+			final OpExp otherExp = (OpExp)other;
+			return (left.equals(otherExp.left) && op.equals(otherExp.op) && right.equals(otherExp.right));
+		} else {
+			return false;
+		}
+	}
+
+	public int hashCode() {
+		return (left.hashCode() + op.hashCode() + right.hashCode());
+	}
+	
+	public String toString() {
+		return ("OpExp(" + left.toString() + ", " + op.toString() + ", " + right.toString() + ")");
+	}
 }
