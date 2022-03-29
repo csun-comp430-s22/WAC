cdfb105797d0aa6eaf7b97a11564235f043cb312
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-03-28 17:43:17 -0700

    added methods for rest of operators + readme

--- src/main/java/WACparser/GreaterThanOp.java
+++ src/main/java/WACparser/GreaterThanOp.java
@@ -1,3 +1,15 @@
 package WACparser;
 
-public class GreaterThanOp implements Op {}
+public class GreaterThanOp implements Op {
+	public boolean equals(final Object other) {
+		return other instanceof GreaterThanOp;
+	}
+	
+	public int hashCode() {
+		return 5;
+	}
+	
+	public String toString() {
+		return "GreaterThanOp";
+	}
+}
