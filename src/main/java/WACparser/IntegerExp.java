8fa8f63db5c860f9587b347ab9e6abc6c019e585
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-03-28 17:22:29 -0700

    added methods for IntegerExp

--- src/main/java/WACparser/IntegerExp.java
+++ src/main/java/WACparser/IntegerExp.java
@@ -6,4 +6,16 @@ public class IntegerExp implements Exp {
 	public IntegerExp(final int value) {
 		this.value = value;
 	}
+	
+	public boolean equals(final Object other) {
+		return (other instanceof IntegerExp && value == ((IntegerExp)other).value);
+	}
+	
+	public int hashCode() {
+		return value;
+	}
+	
+	public String toString() {
+		return "IntegerExp(" + value + ")";
+	}
 }
