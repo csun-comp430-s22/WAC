eb4c40abf3adebaa7cc2e960c72375bddb1fb463
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-03-28 17:29:34 -0700

    added methods for PlusOp

--- src/main/java/WACparser/PlusOp.java
+++ src/main/java/WACparser/PlusOp.java
@@ -1,3 +1,15 @@
 package WACparser;
 
-public class PlusOp implements Op {}
+public class PlusOp implements Op {
+	public boolean equals(final Object other) {
+		return other instanceof PlusOp;
+	}
+	
+	public int hashCode() {
+		return 0;
+	}
+	
+	public String toString() {
+		return "PlusOp";
+	}
+}
