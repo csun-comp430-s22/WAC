8060acef49cdef099e0697e93fb9027153c3168f
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-04-04 18:58:57 -0700

    removing comments

--- src/main/java/WACparser/ClassnameType.java
+++ src/main/java/WACparser/ClassnameType.java
@@ -19,23 +19,3 @@ public class ClassnameType implements Type {
 		return "ClassnameType(" + classname.name + ")";
 	}
 }
-
-/* public class VariableExp implements Exp {
-	public final String name;
-	
-	public VariableExp(final String name) {
-		this.name = name;
-	}
-	
-	public boolean equals(final Object other) {
-		return (other instanceof VariableExp && name.equals(((VariableExp)other).name));
-	}
-	
-	public int hashCode() {
-		return name.hashCode();
-	}
-	
-	public String toString() {
-		return "VariableExp(" + name + ")";
-	}
-} */
