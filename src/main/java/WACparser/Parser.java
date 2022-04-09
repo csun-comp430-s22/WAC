54b9f31fc49fb0116e981aa2bf3d17d3d7ee7d8d
shadowwolf777180 <cesarbarrera173@gmail.com>
2022-04-08 18:09:20 -0700

    fixed merge issue, all test run

--- src/main/java/WACparser/Parser.java
+++ src/main/java/WACparser/Parser.java
@@ -441,8 +441,8 @@ public class Parser {
 			final ParseResult<Stmt> compStmt = new ParseResult<Stmt>(new ExpStmt(compExp.result), compExp.position);
 			return compStmt;
 		}
-	
-
+	}
+}
 	
 
 	/*
--- src/main/java/WACparser/ThisStmt.java
+++ src/main/java/WACparser/ThisStmt.java
@@ -4,26 +4,6 @@ public class ThisStmt implements Stmt {
 
 	public final VariableExp ThisVar;
 	public final VariableExp Var;
-<<<<<<< HEAD
-	
-	public ThisStmt(final VariableExp ThisVar ,final VariableExp Var) {
-		
-		this.Var = Var;
-		this.ThisVar = ThisVar;
-	}
-	public boolean equals(final Object other) {
-		if( other instanceof ThisStmt) 
-		{
-		    final ThisStmt otherThisStmt = (ThisStmt)other;
-		    return(Var.equals(otherThisStmt.Var) && ThisVar.equals(otherThisStmt.ThisVar));
-		}
-		else 
-		{
-			return false;
-		}
-		
-	}
-=======
 
 	public ThisStmt(final VariableExp ThisVar ,final VariableExp Var) {
 
@@ -42,7 +22,6 @@ public class ThisStmt implements Stmt {
 		}
 
 	}
->>>>>>> origin/QA_testing
 	public int hashCode() {
         return (ThisVar.hashCode() + Var.hashCode());
     }
