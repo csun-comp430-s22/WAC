0b576c8f96d9f4219e204eb167ba78fef80f9bb5
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-04-25 16:29:28 -0700

    skeleton for typechecker code

---
+++ src/main/java/WACtypechecker/TypeErrorException.java
@@ -0,0 +1,8 @@
+package WACtypechecker;
+
+public class TypeErrorException extends Exception {
+	
+	public TypeErrorException(final String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/WACtypechecker/Typechecker.java
@@ -0,0 +1,15 @@
+package WACtypechecker;
+
+import WACparser.*;
+
+public class Typechecker {
+	
+	public static Type typeOf(final Exp exp) throws TypeErrorException {
+		if (exp instanceof TrueExp) {
+			return new BooleanType();
+		}
+		else {
+			throw new TypeErrorException("");
+		}
+	}
+}
